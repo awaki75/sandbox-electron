9ff52bd4a6421acfd675968f88fe05872e03fee2
awaki75 <awaki.33s@gmail.com>
2018-07-09 00:52:02 +0900

    Add material-ui

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import Button from '@material-ui/core/Button';
 
 // tslint:disable-next-line:variable-name
-const App = ({ name }: { name: string }) => <div>Hello {name}</div>;
+const App = ({ name }: { name: string }) => (
+  <Button variant="contained" color="primary">
+    Hello {name}
+  </Button>
+);
 
 render(<App name="Taylor" />, document.getElementById('app'));
