109d09055d7c083d9ed5579818873885b47e7710
awaki75 <awaki.33s@gmail.com>
2018-07-27 01:11:52 +0900

    Extract css

--- build/webpack.main.config.js
+++ build/webpack.main.config.js
@@ -1,27 +1,39 @@
 const path = require('path');
 
-/** @type {import('webpack').Configuration} */
-const config = {
-  entry: './src/main/index.ts',
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'main.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.ts'],
-  },
-  target: 'electron-main',
-  node: {
-    __dirname: false,
-  },
+module.exports = env => {
+  return createConfig(env === 'prod');
 };
 
-module.exports = config;
+/**
+ * @param {boolean} isProduction
+ */
+function createConfig(isProduction) {
+  /**
+   * @type {import('webpack').Configuration}
+   */
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/main/index.ts',
+    output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: 'main.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.ts'],
+    },
+    target: 'electron-main',
+    node: {
+      __dirname: false,
+    },
+  };
+
+  return config;
+}
